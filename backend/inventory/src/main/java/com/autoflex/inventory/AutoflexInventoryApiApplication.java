45dd97d3b6712e561aade8d109b6b8ac54bad573
Warley <warleyramiresgoncalves@gmail.com>
2026-02-04 20:12:00 -0300

    Configuração base do backend

---
+++ backend/inventory/src/main/java/com/autoflex/inventory/AutoflexInventoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.autoflex.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoflexInventoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoflexInventoryApiApplication.class, args);
+	}
+
+}
