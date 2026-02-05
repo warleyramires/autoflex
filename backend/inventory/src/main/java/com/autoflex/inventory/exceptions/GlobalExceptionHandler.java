e47d4ea238d80826624f888c8cf8b2185528d28a
Warley <warleyramiresgoncalves@gmail.com>
2026-02-05 15:59:19 -0300

    Criação do GlobalExceptionsHandler

--- backend/inventory/src/main/java/com/autoflex/inventory/exceptions/GlobalExceptionHandler.java
+++ backend/inventory/src/main/java/com/autoflex/inventory/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,28 @@
 package com.autoflex.inventory.exceptions;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleNotFound(EntityNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleBadRequest(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleInternalError(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Internal server error: " + ex.getMessage());
+    }
+
 }
